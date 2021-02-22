7d95f065bcda3b2dcafb283ea728f95fc0fb726a
Brian Burke <burke724@umn.edu>
2021-02-22 12:53:52 -0600

    added host

--- north_american_transport/settings.py
+++ north_american_transport/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 # development
-ALLOWED_HOSTS = ['ad67d80129a0.ngrok.io', 'localhost']
+ALLOWED_HOSTS = ['ad67d80129a0.ngrok.io', 'localhost', 'bburkey49.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATIC_ROOT = 
 
 MEDIA_URL = '/assets/'
 
